5e0d7f943382785e6248ddb1150a43754563ce3f
Al Amin Sheikh <azalamins@gmail.com>
2023-11-22 06:50:17 +0600

    mongoose setup, create user done

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,38 @@
+export type Guardian = {
+  fatherName: string
+  fatherOccupation: string
+  fatherContact: string
+  motherName: string
+  motherOccupation: string
+  motherContact: string
+}
+
+export type Username = {
+  firstName: string
+  middleName: string
+  lastName: string
+}
+
+export type LocalGuardian = {
+  name: string
+  occupation: string
+  contactNo: string
+  address: string
+}
+
+export type Student = {
+  id: string
+  name: Username
+  gender: 'male' | 'female'
+  dateOfBirth?: string
+  email: string
+  contactNo: string
+  emergencyContact: string
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-'
+  presentAddress: string
+  permanentAddress: string
+  guardian: Guardian
+  localGuardian: LocalGuardian
+  profileImg?: string
+  isActive: 'active' | 'blocked'
+}
